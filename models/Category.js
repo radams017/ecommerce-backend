7f3355a828ea35999dad72b3df2dde2371b6812a
Ryan Adams <ryanadams14@gmail.com>
2021-08-10 00:34:28 -0500

    housekeeping

--- models/Category.js
+++ models/Category.js
@@ -8,7 +8,6 @@ const sequelize = require('../config/connection.js');
 class Category extends Model {}
 
 Category.init({
-  // define columns
   id: {
     type: DataTypes.INTEGER,
     allowNull: false,
